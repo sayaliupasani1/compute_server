4074a60274063bd966be2caa83f60e7dc48942c1
Sayali Upasani <sayaliupasani@gmail.com>
2019-09-25 16:33:36 -0700

    Added necessary comments

--- application/app.py
+++ application/app.py
@@ -10,6 +10,17 @@ import random
 import requests
 from authenticator import Keycloak as kc
 
+####
+
+"""Main packages:
+ - Creating and managing docker containers - docker-py
+ - Serving web pages - Flask
+- Authenticating users - Keycloak. This is implemented using requests library to make calls to the keycloak endpoint.
+
+"""
+
+####
+
 docker_client = docker.from_env()
 
 #Creating an object for low-level docker-py API
@@ -20,22 +31,26 @@ assigned_ports = []
 
 @app.route('/')
 def landing():
+	"""
+	On base path, the first step will include checking for access token and validating it.
+	If the token is valid, landing page is rendered without credential authentication.
+	If token is not valid and/or expired, the users are redirected to /login.
+	"""
 	if request.cookies.get('access_token') and request.cookies.get('username'):
-		#print('access_token present')
 		access_token = request.cookies.get('access_token')
 		refresh_token = request.cookies.get('refresh_token')
 		username = request.cookies.get('username')
-		#print('Refresh token in login func: {}'.format(refresh_token))
+		# The verification can result in one of the following: access token valid, refresh token valid, need to re-authenticate user
 		verification_code, verification_value = kc().verify_signature(access_token, refresh_token)
-		#print('verification_value:{}'.format(verification_value))
 		if verification_code == 'username' and verification_value == username:
 			return render_template('landing.html')
+		# If refresh token is valid, users are served with the requested page along with setting a new access token
 		elif verification_code == 'access_token_new':
-			#print('This is new access token')
 			access_token_new = verification_value
 			response = make_response(redirect(url_for('landing')))
 			response.set_cookie('access_token', access_token_new)
 			return response
+		# If access token and refresh token are expired, user is redirected to /login where they pass through complete authentication
 		elif verification_code == 'authenticate':
 			return redirect(url_for('login'))
 	else:
@@ -44,28 +59,41 @@ def landing():
 
 @app.route('/login')
 def login():
-	print('This is login func')
+	"""
+	If users come with authorization code (after keycloak redirection), the landing page is served along \
+	with setting access token. refresh token and username in cookie.
+
+	This function calls get_access_token function to fetch the tokens from Keycloak.
+
+	"""
 	if request.args.get('code'):
 		session_state = request.args.get('session_state')
 		auth_code = request.args.get('code')
-		#print('Authorization code:{}'.format(auth_code))
+		# call the get_access_token for exchanging authorization code for access and refresh tokens
 		access_token, refresh_token, username = kc().get_access_token(auth_code)
 		response = make_response(render_template('landing.html'))
+		# Set the tokens as cookies in the response
 		response.set_cookie('access_token', access_token)
 		response.set_cookie('refresh_token', refresh_token)
 		response.set_cookie('username', username)
-		#print('Access token in landing page func:{}'.format(access_token))
-		#print('Refresh token in landing page func:{}'.format(refresh_token))
 		return response
 	else:
+		# If user request does not have authorization code, they are authenticated - send to keycloak who sets the auth code
 		auth_url = kc().authenticate_user()
 		return redirect(auth_url)
 
 @app.route('/listOfContainers.html')
 def listcon(): 
+	"""
+	This function serves the page for listing currently running user's containers.
+	If users come with access token, the token is manually validated using pyJWT before rendering the page.
+	If the signature is expired, users are redirected to /login where pass through complete authentication process.
+
+	"""
 	if request.cookies.get('access_token'):
 		token = request.cookies.get('access_token')
 		verify_token = kc().verify_login(token)
+		# If access token is valid, verify_login function returns True, and the container list is calculated and served
 		if verify_token:
 			container_dict = {}
 			df_html = "You have no running containers currently."
@@ -84,13 +112,17 @@ def listcon():
 
 @app.route('/containerdetails.html', methods=['GET', 'POST'])
 def get_containerdetails():
+	"""
+	This function takes container details as user input based on which, the container is created.
+	"""
 	if request.method == 'GET':
 		return render_template('containerDetails.html')
 	if request.method == 'POST':
 		#con_details = {}
 		image_name = request.form['image_name']
-		if image_name == 'ubuntu':
+		if image_name == 'ubuntu': # Currently, users are being provided with only two image options
 			image = 'rastasheep/ubuntu-sshd:16.04'
+			# Call create_container function to create a container using specified image/requirements
 			container_data = create_container(image)
 			return render_template('created_container.html', image=image, container_name=container_data[0], container_port=container_data[1])
 		elif image_name == 'apache':
@@ -99,29 +131,49 @@ def get_containerdetails():
 			return render_template('created_container.html', image=image, container_name=container_data[0], container_port=container_data[1])
 
 def create_container(image):
+	"""
+	This function is to create a container with specified image.
+	The container is create with port forwarding on host so that user's can use relevant port to drop into SSH shell of their container.
+	It returns the container details that are being displayed to the user.
+
+	"""
+	# The for loop is to decide the host port. The already assigned ports are tracked in the list.
 	for i in range(1025, 49152):
 		port = random.randint(1025, 49152)
 		if port not in assigned_ports:
 			assigned_ports.append(port)
 			break
+	""" Using docker-py for creating and managing docker containers - Below call creates a container with specified image and port forwarding
+	The remove flag ensures to remove the container after it has been stopped """
 	container = docker_client.containers.run(image, ports = {22:port}, detach=True, remove=True)
 	container_name = container.name
+	# Inspect the created container for specific details to be provided to the user
 	container_port = docker_api.inspect_container(container.id)['NetworkSettings']['Ports']['22/tcp']
 	return container_name, container_port
 
 @app.route('/deletecontainer.html', methods=['GET', 'POST'])
 def deletecontainer():
+	"""
+	This function is to delete specified container. User is asked to input the container ID to be deleted
+	"""
 	if request.method == 'GET':
 		return render_template('containertodelete.html')
 	if request.method == 'POST':
 		container_id = request.form['container_id']
+		# Create a container object for specified container ID
 		cont_obj = docker_client.containers.get(container_id)
+		# Stop the container using object
 		cont_obj.stop()
 		return render_template('deletecontainer.html')
 
 @app.route('/logout')
 def logout():
-	print('you hit flask logout func')
+	"""
+	This function is to logout the users. Called when user's click on the 'logout' button.
+	It makes a call to keycloak authenticator's logout_user function, which ensures that keycloak is aware of user logout.
+	In response, the username, access_token and refresh_token cookies are removed.
+
+	"""
 	logout_uri = kc().logout_user()
 	logout_response = redirect(logout_uri)
 	logout_response.set_cookie('username', expires=0)
@@ -129,6 +181,6 @@ def logout():
 	logout_response.set_cookie('refresh_token', expires=0)
 	return logout_response
 
-
+# Runs flask
 if __name__=='__main__':
 	app.run(debug=True)
--- application/authenticator.py
+++ application/authenticator.py
@@ -6,32 +6,71 @@ import jwt
 
 class Keycloak(object):
 
+	"""
+	Keycloak parameters/Variables
+
+	------------
+
+	base_uri: str
+		Base URL for the keycloak server (For tests: http://localhost:8080)
+	redirect_uri: str
+		Redirection URL to provide after successful authentication
+	realm: str
+		Keycloak realm for this application
+	client_id: str
+		Keycloak client ID for this application
+	client_secret: str
+		Keycloak client secret when using confidential client
+
+	"""
+
 	def authenticate_user(self):
-		print('This is authenticate user function')
-		params = {'client_id': client_id, 'redirect_uri': auth_redirect_uri, 'response_type':'code'}
-		auth_url = 'http://localhost:8080/auth/realms/compute_server/protocol/openid-connect/auth?'
+		"""
+		The function creates a valid authorization endpoint URI.
+		Users are redirected to this URI with required parameters, where Keycloak authenticates the user.
+		In response, keycloak adds authorization code, which is used to fetch tokens
+
+		"""
+		params = {'client_id': client_id, 'redirect_uri': '{}/login'.format(redirect_uri), 'response_type':'code'}
+		auth_url = '{0}/auth/realms/{1}/protocol/openid-connect/auth?'.format(base_uri, realm)
+		# urllib to encode the parameters with URI
 		auth_endpoint_url = auth_url + urllib.parse.urlencode(params)
 		return auth_endpoint_url
 
 	def get_access_token(self, auth_code):
-		token_endpoint_params = {'grant_type':'authorization_code', 'code':auth_code, 'client_id': client_id, 'client_secret': client_secret, 'redirect_uri':auth_redirect_uri}
-		tokens = requests.post('http://localhost:8080/auth/realms/compute_server/protocol/openid-connect/token', data=token_endpoint_params)
+		"""
+		Takes the authorization code and exchanges it for access and refresh tokens.
+
+		"""
+		# Params required with token endpoint URI
+		token_endpoint_params = {'grant_type':'authorization_code', 'code':auth_code, 'client_id': client_id, 'client_secret': client_secret, \
+		'redirect_uri':'{}/login'.format(redirect_uri)}
+		# Make request to Keycloak's token endpoint and pass authorization code in data
+		tokens = requests.post('{0}/auth/realms/{1}/protocol/openid-connect/token'.format(base_uri, realm), data=token_endpoint_params)
 		token_dict = json.loads(tokens.text)
-		#print('token dict:{}'.format(token_dict))
 		access_token = token_dict['access_token']
 		refresh_token = token_dict['refresh_token']
 		user_endpoint_params = {'access_token':access_token}
-		user_info = requests.post('http://localhost:8080/auth/realms/compute_server/protocol/openid-connect/userinfo', data=user_endpoint_params)
-		#print('User info: {}'.format(user_info.text))
+		# Make request to keycloak's user_info endpoint in order to fetch the username. We need to set this in cookie
+		user_info = requests.post('{0}/auth/realms/{1}/protocol/openid-connect/userinfo'.format(base_uri, realm), data=user_endpoint_params)
 		user_info_data = json.loads(user_info.text)
 		username = user_info_data['preferred_username']
 		return access_token, refresh_token, username
 
 	def refresh_access_token(self, refresh_token):
+		"""
+		
+		If the access token is expired and refresh token is valid, the token is validated and new access token is fetched.
+		User's are not required to provide credentials for login.
+		
+		"""
+		# parameters required along with token endpoint. The grant_type is refresh_token, which tells keycloak to validate the  \
+		# refresh token and issue new access token
 		refresh_token_endpoint_params = {'grant_type':'refresh_token', 'refresh_token':refresh_token, 'client_id': client_id, 'client_secret':client_secret}
-		access_token_renewed = requests.post('http://localhost:8080/auth/realms/compute_server/protocol/openid-connect/token', data=refresh_token_endpoint_params)
+		access_token_renewed = requests.post('{0}/auth/realms/{1}/protocol/openid-connect/token'.format(base_uri, realm),\
+		data=refresh_token_endpoint_params)
 		access_token_dict = json.loads(access_token_renewed.text)
-		#print('access_token_dict:{}'.format(access_token_dict))
+		# If the refresh token is expired, Keycloak gives an error. Catch that error and re-authenticate user.
 		if access_token_dict.get('error', False):
 			print('Refresh token seems to be expired.. Authenticate again')
 			return False
@@ -40,21 +79,26 @@ class Keycloak(object):
 			return access_token_new
 
 	def verify_signature(self, access_token, refresh_token):
-		#print('this is in verify_signature')
-		#print(access_token)
+
+		"""
+
+		If the user comes with access token, this verifies the signature. If verification results in error, refresh token is validated.
+		Keycloak's introspect endpoint is used for validating the access token.
+
+		"""
+		# parameters required along with introspect endpoint URI
 		introspect_data = {'token':access_token, 'client_id':client_id, 'client_secret': client_secret}
-		user_info = requests.post('http://localhost:8080/auth/realms/compute_server/protocol/openid-connect/token/introspect', data=introspect_data)
+		user_info = requests.post('{0}/auth/realms/{1}/protocol/openid-connect/token/introspect'.format(base_uri, realm), data=introspect_data)
 		user_info_data = json.loads(user_info.text)
-		#print('user info in verify signature:{}'.format(user_info_data))
-		#print(user_info_data['active'])
+		# If the access token is active, username is returned, which is cross verified with username cookie before serving the request.
 		if user_info_data['active'] == True:
 			verification_value = user_info_data['username']
-			print('verified_username:{}'.format(verification_value))
 			verification_code = 'username'
 			return verification_code, verification_value
+		# If access token is expired and refresh token exists, call the refresh_access_token function
 		elif refresh_token != None:
 			access_token_new = self.refresh_access_token(refresh_token)
-			#print('New access token:{}'.format(access_token_new))
+			# The refresh_access_token function returns False if refresh token is expired and user needs to be re-authenticated
 			if not access_token_new:
 				verification_code = 'authenticate'
 				verification_value = 'Null'
@@ -62,25 +106,34 @@ class Keycloak(object):
 				verification_code = 'access_token_new'
 				verification_value = access_token_new
 			return verification_code, verification_value
+		# If no refresh token exists, authenticate the user
 		else:
 			verification_code = 'authenticate'
 			verification_value = 'Null'
 			return verification_code, verification_value
 
 	def verify_login(self, token):
-		jwk_uri = 'http://localhost:8080/auth/realms/compute_server/protocol/openid-connect/certs'
+
+		"""
+		This function is called for each Flask route. This is to ensure that user's login is verified for each web page.
+		To avoid multiple calls to Keycloak's introspect endpoint, this function performs manual token validation
+
+		"""
+		# Make request to keycloak's jwks endpoint, which returns the Json web key set
+		jwk_uri = '{0}/auth/realms/{1}/protocol/openid-connect/certs'.format(base_uri, realm)
 		jwks = requests.get(jwk_uri).json()
-		#print('This is jwks:{}'.format(jwks))
 		public_keys = {}
+		# Sort all the returned jwk into dictionary keyed with their KID. In this case, we are returned with a single key
 		for jwk in jwks['keys']:
 			kid = jwk['kid']
 			public_keys[kid] = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(jwk))
-		#print('This is public key dict:{}'.format(public_keys))
+		# Get the Kid from access token - it tells which key was used to sign the token
 		kid = jwt.get_unverified_header(token)['kid']
+		# Use the same key to verify signature
 		key = public_keys[kid]
-		#print('This is key:{}'.format(key))
+		# The jwt decode throws an exception if it fails to verify signature. Catch the exception and authenticate user before serving the request.
 		try:
-			jwt_decode = jwt.decode(token, key=key, audience ='compute_server')
+			jwt_decode = jwt.decode(token, key=key, audience ='{}'.format(realm))
 			print(jwt_decode)
 			return True
 		except:
@@ -88,8 +141,15 @@ class Keycloak(object):
 
 
 	def logout_user(self):
-		print('You hit kc logout user func')
-		logout_endpoint_params = {'redirect_uri': logout_redirect_uri}
-		logout_endpoint = 'http://localhost:8080/auth/realms/compute_server/protocol/openid-connect/logout?'
+
+		"""
+
+		When user decides to logout, this function is called.
+		It creates a redirection URI pointing towards keycloak's logout endpoint + required parameters.
+		Users are redirected to this URI and keycloak handles logging out.
+
+		"""
+		logout_endpoint_params = {'redirect_uri': redirect_uri}
+		logout_endpoint = '{0}/auth/realms/{1}/protocol/openid-connect/logout?'.format(base_uri, realm)
 		logout_uri = logout_endpoint + urllib.parse.urlencode(logout_endpoint_params)
 		return logout_uri
